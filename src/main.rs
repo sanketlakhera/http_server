c30e642bf22696af57e3a506322342b5e2a3a16a
Sanket <lakherasanket@gmail.com>
2023-08-16 18:17:02 +0530

    done

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use http::Method;
 use http::Request;
 use server::Server;
+use std::env;
+use website_handler::WebsiteHandler;
 
 mod http;
 mod server;
+mod website_handler;
 
 fn main() {
-    let server = Server::new("127.0.0.1:8080".to_string());
-    server.run();
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+    println!("pub path: {}", public_path);
+    let server = Server::new("127.0.0.1:3000".to_string());
+    server.run(WebsiteHandler::new(public_path));
 }
--- src/server.rs
+++ src/server.rs
@@ -1,9 +1,18 @@
-use crate::http::Request;
+use crate::http::{ParseError, Request, Response, StatusCode};
 use std::convert::TryFrom;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 pub struct Server {
     addr: String,
+}
+
+pub trait Handler {
+    fn handle_request(&mut self, request: &Request) -> Response;
+
+    fn handle_bad_request(&mut self, e: &ParseError) -> Response {
+        println!("Failed to parse request: {}", e);
+        Response::new(StatusCode::BadRequest, None)
+    }
 }
 
 impl Server {
@@ -11,7 +20,7 @@ impl Server {
         Self { addr }
     }
 
-    pub fn run(self) {
+    pub fn run(self, mut handler: impl Handler) {
         println!("Listening on {}", self.addr);
 
         let listener = TcpListener::bind(&self.addr).unwrap();
@@ -26,13 +35,13 @@ impl Server {
                                 "Received a request: {} _\n",
                                 String::from_utf8_lossy(&buffer)
                             );
-                            match Request::try_from(&buffer[..]) {
-                                Ok(request) => {
-                                    dbg!(request);
-                                }
-                                Err(e) => {
-                                    println!("Failed to parse request {}", e);
-                                }
+                            let response = match Request::try_from(&buffer[..]) {
+                                Ok(request) => handler.handle_request(&request),
+                                Err(e) => handler.handle_bad_request(&e),
+                            };
+
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response: {}", e);
                             }
                         }
                         Err(e) => {
